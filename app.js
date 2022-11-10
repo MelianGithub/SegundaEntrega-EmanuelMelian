337c9998f49cbcb076bdbc2980b3ffab36702209
MelianGithub <e_melian04@hotmail.com>
2022-11-10 08:52:14 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,106 @@
+const productos = [
+    {
+        id: 1,
+        marca: "Lenovo",
+        modelo: "Legion 5 Pro",
+        cpu: "INTEL CORE I9",
+        gpu: "RTX 3080",
+        RAM: 12,
+        disco: 500,
+        precio: 2000,
+        stock: 10
+    },
+    {
+        id: 2,
+        marca: "Acer",
+        modelo: "Nitro",
+        cpu: "AMD RYZEN 9",
+        gpu: "RX 6000",
+        RAM: 16,
+        disco: 500,
+        precio: 8000,
+        stock: 5
+    },
+    {
+        id: 3,
+        marca: "Asus",
+        modelo: "TUF Gaming",
+        cpu: "INTEL CORE I5",
+        gpu: "RTX 2060",
+        RAM: 32,
+        disco: 1000,
+        precio: 5000,
+        stock: 0
+    },
+    {
+        id: 4,
+        marca: "Lenovo",
+        modelo: "Alienware",
+        cpu: "INTEL CORE I9",
+        gpu: "RXT 3080",
+        RAM: 64,
+        disco: 1500,
+        precio: 14000,
+        stock: 2
+    },
+    {
+        id: 5,
+        marca: "Lenovo",
+        modelo: "Alienware",
+        cpu: "INTEL CORE I9",
+        gpu: "RXT 3080",
+        RAM: 64,
+        disco: 1500,
+        precio: 14000,
+        stock: 4
+    },
+    {
+        id: 6,
+        marca: "Lenovo",
+        modelo: "Alienware",
+        cpu: "INTEL CORE I9",
+        gpu: "RXT 3080",
+        RAM: 64,
+        disco: 1500,
+        precio: 14000,
+        stock: 12
+    },
+    {
+        id: 7,
+        marca: "Lenovo",
+        modelo: "Alienware",
+        cpu: "INTEL CORE I9",
+        gpu: "RXT 3080",
+        RAM: 64,
+        disco: 1500,
+        precio: 14000,
+        stock: 0
+    }
+]
+function buscarStock(){
+ let productoElegido = prompt("Ingrese marca o modelo que desee:");
+
+ const buscar = productos.filter(producto => {
+    return productoElegido.toLocaleLowerCase() === producto.marca.toLocaleLowerCase();
+});
+if(productoElegido === buscar ){
+    console.log(buscar);
+}else{
+    console.log("no hay chance");
+}
+}
+
+
+buscarStock();
+
+
+// -----------------------------------------------------
+// este es el que funciona bien
+
+// let productoElegido = prompt("Ingrese marca o modelo que desee:");
+
+// const buscar = productos.filter(producto => {
+//     return productoElegido.toLocaleLowerCase() === producto.marca.toLocaleLowerCase();
+// });
+
+// ---------------------------------------
